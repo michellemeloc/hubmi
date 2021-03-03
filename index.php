0284c04bbeae5751703ac507255d8dfcfd160e64
Michelle Cavalcante <michellemelo.c@gmail.com>
2021-03-03 16:19:09 -0300

    cores no html

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
    <body>
       <h1>Hubmi</h1>
       <p>Gerando soluções para você</p>
-      <div style="width:100%;display:flex;height:80px">
-         <div style="width:50%;backgroud:blue"></div>
-         <div style="width:50%;backgroud:red"></div>
+      <div style="width:100%; display:flex; height:80px">
+         <div style="width:50%; background:blue"></div>
+         <div style="width:50%; background:red"></div>
       </div>
    </body>
 </html>
